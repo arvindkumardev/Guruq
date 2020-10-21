8da92b64215f8e1621e85cc6e6b420028eb09a9c
arunmeena <arun.meena@gmail.com>
2020-10-21 19:38:00 +0530

    auth token from asyncstorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
-// import { ApolloProvider } from '@apollo/client';
-
 import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import apolloClient from './apollo/apollo';
 import AppStack from './routes/AppRoutes';
 
--- src/containers/dashboard/dashboard.js
+++ src/containers/dashboard/dashboard.js
@@ -1,8 +1,13 @@
-import { StatusBar, Text, View } from 'react-native';
+import {
+  Button, SafeAreaView, StatusBar, Text, View, Alert
+} from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
 import commonStyles from '../../common/styles';
 import Colors from '../../theme/colors';
+import { LOCAL_STORAGE_DATA_KEY } from '../../utils/constants';
+import routeNames from '../../routes/ScreenNames';
 
 function dashboard(props) {
   const navigation = useNavigation();
@@ -13,14 +18,28 @@ function dashboard(props) {
     navigation.goBack();
   };
 
+  const logout = () => {
+      Alert.alert('Logout!');
+    AsyncStorage.removeItem(LOCAL_STORAGE_DATA_KEY.USER_TOKEN).then(() => {
+      navigation.navigate(routeNames.LOGIN);
+    });
+  };
+
   return (
-    <View style={[commonStyles.mainContainer, { backgroundColor: Colors.white }]}>
+    <SafeAreaView style={[commonStyles.mainContainer, { backgroundColor: Colors.white }]}>
       <StatusBar barStyle="light-content" />
       {/* <Icon onPress={() => onBackPress()} type="MaterialIcons" name="keyboard-backspace" style={{ marginLeft: 16, marginTop: 58, color: Colors.white }} /> */}
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>This is Dashboard</Text>
+
+        <View style={{ marginTop: 40 }}>
+          <Button
+            title="Logout"
+            onPress={() => logout()}
+          />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
