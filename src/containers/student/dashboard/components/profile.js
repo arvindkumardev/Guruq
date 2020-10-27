544cc7421bfcef3cce9379a7f18b1df7325f806e
Rajan Dagar <v-rajan@damacgroup.com>
2020-10-27 09:33:48 +0530

    App flow changes

--- src/containers/student/dashboard/components/profile.js
+++ src/containers/student/dashboard/components/profile.js
@@ -17,11 +17,34 @@ function profile() {
   ]);
 
   const renderItem = (item) => {
-    <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'stretch', paddingLeft: RfW(16) }}>
-        <IconWrapper iconImage={Images.personal} />
-        <Text style={{ color: Colors.darktitle }}>{item.name}</Text>
-    </View>
-  }
+    <View style={styles.userMenuParentView}>
+      <IconWrapper iconImage={Images.personal} />
+      <Text style={{ color: Colors.darktitle }}>{item.name}</Text>
+    </View>;
+  };
+
+  const renderActionIcons = () => {
+    return (
+      <View style={[styles.userMenuParentView, { justifyContent: 'space-evenly' }]}>
+        <View style={styles.actionIconParentView}>
+          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.heart} />
+          <Text style={styles.actionText}>Favourites</Text>
+        </View>
+        <View style={styles.actionIconParentView}>
+          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.bell} />
+          <Text style={styles.actionText}>Notification</Text>
+        </View>
+        <View style={styles.actionIconParentView}>
+          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.qpoint} />
+          <Text style={styles.actionText}>Q Points</Text>
+        </View>
+        <View style={styles.actionIconParentView}>
+          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.cart} />
+          <Text style={styles.actionText}>Cart</Text>
+        </View>
+      </View>
+    );
+  };
 
   return (
     <View style={commonStyles.mainContainer}>
@@ -30,48 +53,19 @@ function profile() {
       <View style={styles.userDetailsView}>
         <Image style={styles.userIcon} source={Images.user} />
         <View style={{ flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'stretch' }}>
-          <Text
-            style={{ marginTop: RfH(8), marginLeft: RfW(12), fontSize: 18, fontWeight: '600', color: 'rgb(25,24,24)' }}>
-            Sheena Jain
-          </Text>
-          <Text style={{ marginLeft: RfW(12), marginTop: RfH(2), color: 'rgb(129,129,129)' }}>+91-9876543210</Text>
-          <Text style={{ marginLeft: RfW(12), color: 'rgb(129,129,129)' }}>GURUQS21223I</Text>
+          <Text style={styles.userName}>Sheena Jain</Text>
+          <Text style={styles.userMobDetails}>+91-9876543210</Text>
+          <Text style={styles.userMobDetails}>GURUQS21223I</Text>
         </View>
       </View>
-      <View style={{ flex: 1, borderBottomColor: Colors.inputLabel, borderBottomWidth: 0.5, marginTop: RfH(16) }} />
-      <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', marginTop: RfH(16) }}>
-        <View style={{ flex: 0.25, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.heart} />
-          <Text style={{ fontSize: 12, color: Colors.inputLabel, marginTop: RfH(8) }}>Favourites</Text>
-        </View>
-        <View style={{ flex: 0.25, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.bell} />
-          <Text style={{ fontSize: 12, color: Colors.inputLabel, marginTop: RfH(8) }}>Notification</Text>
-        </View>
-        <View style={{ flex: 0.25, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.qpoint} />
-          <Text style={{ fontSize: 12, color: Colors.inputLabel, marginTop: RfH(8) }}>Q Points</Text>
-        </View>
-        <View style={{ flex: 0.25, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <IconWrapper iconHeight={RfH(24)} iconWidth={RfW(24)} iconImage={Images.cart} />
-          <Text style={{ fontSize: 12, color: Colors.inputLabel, marginTop: RfH(8) }}>Cart</Text>
-        </View>
-      </View>
-      <View style={{ flex: 1, borderBottomColor: Colors.inputLabel, borderBottomWidth: 0.5, marginTop: RfH(16) }} />
-      <View
-        style={{
-          flex: 1,
-          marginTop: RfH(16),
-          flexDirection: 'row',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-        }}>
+      <View style={styles.separatorView} />
+      {renderActionIcons()}
+      <View style={styles.separatorView} />
+      <View style={styles.userMenuParentView}>
         <IconWrapper styling={{ flex: 0.1 }} iconHeight={RfH(16)} iconWidth={RfW(16)} iconImage={Images.profile} />
-        <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'stretch' }}>
-          <Text style={{ fontSize: 16, fontWeight: '600', color: Colors.darktitle, marginLeft: RfW(12) }}>
-            My Account
-          </Text>
-          <Text numberOfLines={1} ellipsizeMode="tail" style={{ marginLeft: RfW(12), color: Colors.inputLabel }}>
+        <View style={styles.menuItemParentView}>
+          <Text style={styles.menuItemPrimaryText}>My Account</Text>
+          <Text numberOfLines={1} ellipsizeMode="tail" style={styles.menuItemSecondaryText}>
             Personal Details , Address , Parent Details , Education
           </Text>
         </View>
@@ -84,7 +78,7 @@ function profile() {
           />
         </TouchableWithoutFeedback>
       </View>
-        <View style={{ flex: 1, borderBottomColor: Colors.inputLabel, borderBottomWidth: 0.5, marginTop: RfH(16) }} />
+      <View style={styles.separatorView} />
       <View>
         {isAccountMenuOpen && (
           <FlatList
@@ -94,7 +88,7 @@ function profile() {
             keyExtractor={(item, index) => index.toString()}
           />
         )}
-    </View>
+      </View>
     </View>
   );
 }
--- src/containers/student/dashboard/styles.js
+++ src/containers/student/dashboard/styles.js
@@ -13,22 +13,72 @@ const styles = StyleSheet.create({
     marginTop: RfH(4),
     color: Colors.primaryButtonBackground,
   },
-  myProfileText:{
-    marginTop: RfH(48), 
-    fontSize: 20, 
-    fontWeight: 'bold' 
+  myProfileText: {
+    marginTop: RfH(48),
+    fontSize: 20,
+    fontWeight: 'bold',
   },
-  userDetailsView:{
+  userDetailsView: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
-     alignItems: 'stretch',
-      marginTop: RfH(24)
+    alignItems: 'stretch',
+    marginTop: RfH(24),
   },
-  userIcon:{
-    height: RfH(82), 
-    width: RfW(82), 
-    borderRadius: 8 
-
-  }
+  userIcon: {
+    height: RfH(82),
+    width: RfW(82),
+    borderRadius: 8,
+  },
+  userName: {
+    marginTop: RfH(8),
+    marginLeft: RfW(12),
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'rgb(25,24,24)',
+  },
+  userMobDetails: {
+    marginLeft: RfW(12),
+    color: 'rgb(129,129,129)',
+  },
+  separatorView: {
+    flex: 1,
+    borderBottomColor: Colors.inputLabel,
+    borderBottomWidth: 0.5,
+    marginTop: RfH(16),
+  },
+  userMenuParentView: {
+    flex: 1,
+    marginTop: RfH(16),
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  menuItemParentView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  menuItemPrimaryText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: Colors.darktitle,
+    marginLeft: RfW(12),
+  },
+  menuItemSecondaryText: {
+    marginLeft: RfW(12),
+    color: Colors.inputLabel,
+  },
+  actionIconParentView: {
+    flex: 0.25,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  actionText: {
+    fontSize: 12,
+    color: Colors.inputLabel,
+    marginTop: RfH(8),
+  },
 });
 export default styles;
