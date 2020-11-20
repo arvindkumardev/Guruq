25ab525f3884e9673f3272cba7893fef8ef25466
arun <arun@rhatechnology.com>
2020-11-20 17:08:25 +0530

    booking fixes

---
+++ src/containers/student/class.query.js
@@ -0,0 +1,16 @@
+// query {
+//   getAvailability(
+//     tutorAvailability: {
+//       tutorId: 45725
+//       startDate: "2020-11-01T00:00:00+05:30"
+//       endDate: "2020-11-30T00:00:00+05:30"
+//     }
+//   ) {
+//     id
+//     active
+//     startDate
+//     endDate
+//   }
+// }
+
+
