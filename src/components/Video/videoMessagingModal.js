81b71ddbc7ffec257bd0189bfb1f9c33ada13428
arun <arun@rhatechnology.com>
2020-11-29 12:57:03 +0530

    video ui changes

--- src/components/Video/videoMessagingModal.js
+++ src/components/Video/videoMessagingModal.js
@@ -23,7 +23,6 @@ const VideoMessagingModal = (props) => {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
