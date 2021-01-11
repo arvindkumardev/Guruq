9a2f640c09bfedcbcfcd8b6ca0dbb6e0d1ab22ec
arunmeena <arun.meena@gmail.com>
2021-01-11 19:42:20 +0530

    fixes

--- src/containers/onlineClass/components/chat.graphql.js
+++ src/containers/onlineClass/components/chat.graphql.js
@@ -5,8 +5,8 @@ export const NEW_CHAT_MESSAGE = gql`
   subscription {
     chatMessageSent {
       id
-      uuid
       channel
+      isSystem
       createdBy {
         id
         firstName
@@ -26,8 +26,8 @@ export const GET_CHAT_MESSAGES = gql`
   query GetChatMessages($channelName: String!) {
     getChatMessages(channel: $channelName) {
       id
-      uuid
       channel
+      isSystem
       createdBy {
         id
         firstName
@@ -47,8 +47,8 @@ export const SEND_CHAT_MESSAGE = gql`
   mutation SendChatMessage($chatMessageDto: ChatMessageDto!) {
     sendChatMessage(chatMessageDto: $chatMessageDto) {
       id
-      uuid
       channel
+      isSystem
       createdBy {
         id
         firstName
